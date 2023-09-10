079650c555a2aed38f52681656e7548d56f1ead5
Dennis <dennisfarias3@gmail.com>
2023-09-09 23:43:55 -0300

    commit

--- ex78.py
+++ ex78.py
@@ -1,22 +1,24 @@
+maior = menor = 0
 numeros = list()
 for c in range(0, 5):
     numeros.append(int(input('Digite um valor para a posição {}: '.format(c))))
-
+    if c == 0:
+        maior = numeros[c]
+        menor = numeros[c]
+    else:
+        if numeros[c] > maior:
+            maior = numeros[c]
+        if numeros[c] < menor:
+            menor = numeros[c]
 print('=-' * 30)
 print('Você digitou os valores {}'.format(numeros))
-
-for pos, num in enumerate(numeros):
-    if pos == 0:
-        maior = numeros[pos]
-        menor = numeros[pos]
-        posiçãomaior = pos
-        posiçãomenor = pos
-    else:
-        if numeros[pos] >= maior:
-            maior = numeros[pos]
-            posiçãomaior = pos
-        if numeros[pos] <= menor:
-            menor = numeros[pos]    
-            posiçãomenor = pos
-print('O maior valor digitado foi {} nas posições {:.<4}'.format(maior, posiçãomaior))
-print('O menor valor digitado foi {} nas posições {:.<4}'.format(menor, posiçãomenor))
+print('O maior valor digitado foi {} nas posições '.format(maior), end='')
+for i, v in enumerate(numeros):
+    if v == maior:
+        print('{}... '.format(i),end=''),
+print()
+print('O menor valor digitado foi {} nas posições '.format(menor), end='')
+for i, v in enumerate(numeros):
+    if v == menor:
+        print('{}... '.format(i),end='')
+print()
--- ex80.py
+++ ex80.py
@@ -1,28 +1,16 @@
 numeros = list()
 for c in range(5):
     num = int(input('Digitd um valor: '))
-    if c == 0:
+    if c == 0 or num >= numeros[len(numeros)-1]:
         numeros.append(num)
-        print('Valor adicionado ao final da lista...')
-    if c == 1:
-        if num < numeros[0]:
-            print('Adicionado na posição 0 da lista...')
-            numeros.insert(0, num)
-        else:
-            print('Valor adicionado ao final da lista...')
-            numeros.append(num)
-    if c == 2:
-        if num > numeros[0] and num > numeros[1]:
-            print('Valor adicionado ao final da lista...')
-            numeros.append(num)
-        elif num > numeros[0] and num < numeros[1]:
-            print('Valor adicionado na posição 1 da lista...')
-            numeros.insert(1, num)
-        elif num > numeros[1] and num < numeros[0]:
-            numeros.insert(1, num)
-        else:
-            print('Valor adicionado na posição 0 da lista')
-            numeros.insert(0, num)
-    
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(numeros):
+            if num <= numeros[pos]:
+                numeros.insert(pos, num)
+                print('Adicionado na posição {} da lista...'.format(pos))
+                break
+            pos += 1
 print('-=' * 30)
 print('Os valores digitados em ordem foram {}'.format(numeros))
