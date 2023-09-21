d053df11fd5045b97abd8ae340f4c5acce23210d
Dennis <dennisfarias3@gmail.com>
2023-09-20 23:50:08 -0300

    commit

---
+++ ex92.py
@@ -0,0 +1,16 @@
+from datetime import date
+ficha = {}
+ficha['nome'] = str(input('Nome: '))
+ficha['nasc'] = int(input('Ano de Nascimento: '))
+idade = date.today().year - ficha['nasc']
+ficha['idade'] = idade
+ficha['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if ficha['ctps'] != 0 and ficha['ctps'] > 0:
+    ficha['contratação'] = int(input('Ano de contratação: '))
+    ficha['salário'] = float(input('Salário: R$ '))
+    idadetrabalho = ficha['contratação'] - ficha['nasc']
+    ficha['aposentadoria'] = idadetrabalho + 35
+del ficha['nasc']
+print('-=' * 30)
+for k, v in ficha.items():
+    print(f'{k} tem o valor {v}')
