40bba41855667fcf4437ce21732ce59b9018ab93
Dennis <dennisfarias3@gmail.com>
2023-09-22 00:17:45 -0300

    commit

--- ex94.py
+++ ex94.py
@@ -1,9 +1,12 @@
 pessoa = {}
 listadepessoas = []
 somaidade = mediaidade = 0
+mulheres = []
 while True:
     pessoa['nome'] = str(input('Nome: '))
     pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()
+    if pessoa['sexo'] == 'F':
+        mulheres.append(pessoa['nome'])
     pessoa['idade'] = int(input('Idade: '))
     somaidade += pessoa['idade']
     resp = str(input('Quer continuar? [S/N] '))
@@ -14,18 +17,12 @@ mediaidade = somaidade / len(listadepessoas)
 print('-=' * 30)
 print(f'- O grupo tem {len(listadepessoas)} pessoas.')
 print(f'- A média de idade é de {mediaidade:.2f} anos.')
-print('- As mulheres cadastradas foram: ',end='')
-for c in range(len(listadepessoas)):
-    if listadepessoas[c]:
-        c += 1
-        if pessoa['sexo'] == 'F':
-            print(f'{pessoa["nome"]} ',end='')
-print()
+print(f'- As mulheres cadastradas foram: {mulheres}')
 print('- Lista das pessoas que estão acima da média: ')
 for p in listadepessoas:
+    print()
     if pessoa['idade'] > mediaidade:
         for k, v in pessoa.items():
-            print()
-            print(f'{k} = {v};')
-
+            print(f'{k} = {v}; ',end='')
+print()
 print('<< ENCERRADO >>')
