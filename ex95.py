a17fc00e35f22ed1898a417e1f3d196035866ae1
Dennis <dennisfarias3@gmail.com>
2023-09-25 23:05:16 -0300

    commit

--- ex95.py
+++ ex95.py
@@ -2,30 +2,37 @@ listajogadores = []
 ficha = {}
 gols = []
 while True:
-    print('-' * 30) 
+    print('-' * 50) 
     ficha['nome'] = str(input('Nome do Jogador: '))
     qtpartidas = int(input('Quantas partidas {} jogou? '.format(ficha['nome'])))
     for c in range(0, qtpartidas):
-        gols.append(int(input('Quantos gols na partida {}? '.format(c+1))))
+        gols.append(int(input('  Quantos gols na partida {}? '.format(c+1))))
     ficha['gols'] = gols[:]
     ficha['total'] = sum(gols)
     listajogadores.append(ficha.copy())
     gols.clear()
-    resp = str(input('Quer continuar? [S/N] '))
-    if resp in 'Nn':
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
         break
 print('-=' * 30)
-print(f'{"cod":<5}{"nome":<10}{"gols":<10}{"total":>5}')
+print(f'{"cod":<5}{"nome":<15}{"gols":<15}{"total":>5}')
 for c in range(len(listajogadores)):
-    print(f'{c}   {listajogadores[c]["nome"]}     {listajogadores[c]["gols"]}         {listajogadores[c]["total"]}')
+    print(f'{c:<5}{str(listajogadores[c]["nome"]):<15}{str(listajogadores[c]["gols"]):<15}{str(listajogadores[c]["total"]):>5}')
 while True:
-    dados = int(input('Mostrar dados de qual jogador? (999 interrompe) '))
-    if dados == 999:
-        break
-    if dados >= len(listajogadores):
-        print(f'ERRO! Não existe jogador com código {dados}! Tente novamente')
-        print('-' * 30)
+    while True:
+        print('-' * 50)
         dados = int(input('Mostrar dados de qual jogador? (999 interrompe) '))
+        if dados == 999:
+            break
+        if dados < len(listajogadores):
+            break
+        print(f'ERRO! Não existe jogador com código {dados}! Tente novamente!')
+    if dados == 999:
+            break
     print(f'-- LEVANTAMENTO DO JOGADOR {listajogadores[dados]["nome"]}:')
     for c in range(len(listajogadores[dados]['gols'])):
         print(f'   No jogo {c+1} fez {listajogadores[dados]["gols"][c]} gols.')
