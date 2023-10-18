151191212dc065fb4bacc5583a923551ba391389
Dennis <dennisfarias3@gmail.com>
2023-10-18 01:00:32 -0300

    commit

--- ex106.py
+++ ex106.py
@@ -1,24 +1,39 @@
-def manual():
-    comando = ''
-    while True:
-        msg = 'SISTEMA DE AJUDA PyHELP'
-        print('\033[0;30;43m~' * 30)
-        print(f'{msg:^30}')
-        print('~' * 30)
-        comando = str(input('\033[mFunção ou Biblioteca > '))
-        if comando == 'fim':
-            print('\033[41m~~~~~~~~~~')
-            print(' ATÉ LOGO!')
-            print('~~~~~~~~~~\033[m')
-            break
-        print('\033[0;30;44m~' * 40)
-        msg2 = f'Acessando o manual do comando {comando}'
-        print(f'{msg2:^40}')
-        print('~' * 40)
-        print('\033[m')
-        print(f'\033[7m')
-        print(help(comando))
-        print('\033[m')
+from time import sleep
+c = ('\033[m',          # 0 - sem cores
+     '\033[0;30;41m',   # 1 - vermelho
+     '\033[0;30;42m',   # 2 - verde
+     '\033[0;30;43m',   # 3 - amarelo
+     '\033[0;30;44m',   # 4 - azul
+     '\033[0;30;45m',   # 5 - roxo
+     '\033[7;30m'       # 6 - branco
+     );
 
 
-manual()
+def ajuda(com):
+    título(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def título(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+
+# Programa Principal
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou Biblioteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+título('ATÉ LOGO!', 1)
