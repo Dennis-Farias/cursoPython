ac36c11a7ca9fdcf0559b8061f27ad7245c60c73
Dennis <dennisfarias3@gmail.com>
2023-11-04 17:16:52 -0300

    finalizando o mundo 3

--- ex113.py
+++ ex113.py
@@ -1,39 +1,29 @@
 def leiaInt(msg):
-    try:
-        ok = False
-        valor = 0
-        while True:
-            n = str(input(msg))
-            if n.isnumeric():
-                valor = int(n)
-                ok = True
-            else:
-                print('\033[0;31mERRO: por favor, digite um número inteiro válido.\033[m')
-            if ok:
-                break
-        return valor
-    except KeyboardInterrupt:
-        print('\033[0;31mUsuário preferiu não digitar esse número.\033[m')
-        return 0
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[0;31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
 
 def leiaFloat(msg):
-    try:
-        ok = False
-        valor = 0
-        while True:
-            n = str(input(msg))
-            if n.replace('.', '', 1).isdigit():
-                valor = float(n)
-                ok = True
-            else:
-                print('\033[0;31mERRO: por favor, digite um número real válido.\033[m')
-            if ok:
-                break
-        return valor
-    except KeyboardInterrupt:
-        print('\033[0;31mUsuário preferiu não digitar esse número.\033[m')
-        return 0
-    
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[0;31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
 
 inteiro = leiaInt('Digite um Inteiro: ') 
 real = leiaFloat('Digite um Real: ')
--- ex114.py
+++ ex114.py
@@ -1,14 +1,9 @@
-import requests
+import urllib
+import urllib.request
 
-def siteAcessível(url):
-    try:
-        response = requests.get(url)
-        # Verificar o código de status da resposta
-        if response.status_code == 200:
-            print("\033[0;33mConsegui acessar o site Pudim com sucesso!\033[m")
-    except requests.exceptions.RequestException as e:
-        print("\033[0;31mO site Pudim não está acessível no momento.\033[m")
-
-# Exemplo de uso
-urlsite = "http://pudim.com.br/"
-siteAcessível(urlsite)
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso!')
